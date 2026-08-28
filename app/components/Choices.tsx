98db48b339ed996bde2845ad366459afcefe10f3
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-08-28 14:22:52 +0530

    feat: added decision made page

--- app/components/Choices.tsx
+++ app/components/Choices.tsx
@@ -3,6 +3,7 @@ import { Inter, JetBrains_Mono } from "next/font/google";
 import { scenarios } from "../game/scenario"
 import { useState } from "react";
 import { useGameStore } from '../store/GameStore';
+import DecisionMade from "./DecisionMade";
 const inter = Inter({
     subsets: ['latin']
 })
@@ -14,37 +15,47 @@ export default function Choices() {
     const [selectedChoice, setSelectedChoice] = useState<string | null>(null);
     const GameState = useGameStore((state) => state.gameState);
     const applyEffects = useGameStore((state) => state.applyEffects);
+    const selectedChoiceData = scenario.choices.find(
+        (choice) => choice.id === selectedChoice
+    )
     return (
-        <div className="mt-12 mx-24 w-full flex gap-12 items-center text-center justify-center max-w-3xl">
-            <div className=" w-full flex flex-col gap-5">
-                {scenario.choices.map((choice) => (
-                    <button
-                        disabled={selectedChoice !== null}
-                        key={choice.id}
-                        onClick={() => {
-                            if (selectedChoice) return;
-                            setSelectedChoice(choice.id);
-                            applyEffects(choice.effects)
-                        }} className={`${inter.className} rounded-xl  border  cursor-pointer disabled:cursor-not-allowed disabled:border-[#B9CCB2] hover:border-[#B3C5FF] p-6 text-left transition-all flex justify-between items-center text-center ${selectedChoice === choice.id ? "border-[#B3C5FF] bg-[#B3C5FF]/5" : "border-[#B9CCB2] hover:border-[#92ca7e]"}`}>
-                        <div>
-                            <h3 className={`text-xl font-bold text-[#FFB4AB] capitalize`}>
-                                {choice.title}
-                            </h3>
-                            <p className={`${jetBrainsMono.className} mt-2 text-sm leading-6 text-[#B9CCB2]`}>{choice.description}</p>
-                        </div>
-                        <div className={`text-xs flex gap-6 uppercase ${jetBrainsMono.className}`}>
-                            <div className="border-r border-[#E5E2E3] pr-5 flex flex-col gap-2 ">
-                                <p className="text-[#FFB4AB]">Burn ${choice.effects.cash}</p>
-                                <p className="text-[#B3C5FF]">Employees {choice.effects.employee}</p>
+        <>
+            <div className="mt-12 mx-24 w-full flex gap-12 items-center text-center justify-center max-w-3xl">
+                <div className=" w-full flex flex-col gap-5">
+                    {scenario.choices.map((choice) => (
+                        <button
+                            disabled={selectedChoice !== null}
+                            key={choice.id}
+                            onClick={() => {
+                                if (selectedChoice) return;
+                                setSelectedChoice(choice.id);
+                                applyEffects(choice.effects)
+                            }} className={`${inter.className} rounded-xl  border  cursor-pointer disabled:cursor-not-allowed  hover:border-[#B3C5FF] p-6 text-left transition-all flex justify-between items-center text-center ${selectedChoice === choice.id ? "border-[#B3C5FF] hover:border-[#B3C5FF] bg-[#B3C5FF]/5" : "border-[#B9CCB2] hover:border-[#92ca7e]"}`}>
+                            <div>
+                                <h3 className={`text-xl font-bold text-[#FFB4AB] capitalize`}>
+                                    {choice.title}
+                                </h3>
+                                <p className={`${jetBrainsMono.className} mt-2 text-sm leading-6 text-[#B9CCB2]`}>{choice.description}</p>
                             </div>
-                            <div className="border-r flex flex-col gap-2 ">
-                               <p className="text-[#00E639]">Morale {choice.effects.morale}</p>
-                                <p className="text-[#B3C5FF]">Runway {choice.effects.runway}</p>
+                            <div className={`text-xs flex gap-6 uppercase ${jetBrainsMono.className}`}>
+                                <div className="border-r border-[#E5E2E3] pr-5 flex flex-col gap-2 ">
+                                    <p className="text-[#FFB4AB]">Burn ${choice.effects.cash}</p>
+                                    <p className="text-[#B3C5FF]">Employees {choice.effects.employee}</p>
+                                </div>
+                                <div className="border-r flex flex-col gap-2 ">
+                                    <p className="text-[#00E639]">Morale {choice.effects.morale}</p>
+                                    <p className="text-[#B3C5FF]">Runway {choice.effects.runway}</p>
+                                </div>
                             </div>
-                        </div>
-                    </button>
-                ))}
+                        </button>
+                    ))}
+                </div>
             </div>
-        </div>
+            {
+                selectedChoiceData && (
+                    <DecisionMade choice={selectedChoiceData} onNext={() => setSelectedChoice(null)} />
+                )
+            }
+        </>
     )
 }
---
+++ app/components/DecisionMade.tsx
@@ -0,0 +1,70 @@
+import { ChevronRight } from "lucide-react";
+import { Inter, JetBrains_Mono } from "next/font/google";
+
+type DecisionResultProps = {
+    choice: {
+        id: string;
+        title: string;
+        description: string;
+        effects: {
+            cash: number;
+            employee: number;
+            morale: number;
+            runway: number;
+        }
+    };
+    onNext: () => void;
+}
+const inter = Inter({
+    subsets: ['latin']
+})
+const jetBrainsMono = JetBrains_Mono({
+    subsets: ['latin']
+})
+export default function DecisionMade({
+    choice,
+    onNext
+}: DecisionResultProps) {
+    return (
+        <div className="fixed inset-0  flex-col bg-black/70 flex text-center justify-center items-center backdrop-blur-sm  z-50">
+            <p className={`text-xs uppercase text-[#E5E2E3] ${jetBrainsMono.className} font-medium`}>Decision made</p>
+            <h2 className={`mt-4 text-4xl font-bold ${inter.className} text-[#00E639]`}>{choice.title}</h2>
+            <p className={`mt-4 font-light text-[#B3C5FF] ${inter.className} text-sm`}>{choice.description}</p>
+            <div className={`mt-4 border-[#E5E2E3] border-t w-150`} />
+            <div className={`flex  mt-4  uppercase text-sm ${jetBrainsMono.className} items-center text-center ${choice.effects.cash > 0  ? "text-[#00E639]" : "text-[#FFB4AB]"} `}>
+             
+                <p className="w-28 text-left">Cash($)</p>
+                <p className="w-28 text-left">
+                    {choice.effects.cash > 0 ? "+" : ""}
+                    {choice.effects.cash.toLocaleString()}
+                </p>
+            </div>
+            <div className={`flex uppercase mt-2 text-sm ${jetBrainsMono.className} items-center text-center ${choice.effects.employee > 0  ? "text-[#00E639]" : "text-[#FFB4AB]"} `}>
+                <p className="w-28 text-left">Employees</p>
+                <p className="w-28 text-left">
+                    {choice.effects.employee > 0 ? "+" : ""}
+                    {choice.effects.employee}
+                </p>
+            </div>
+            <div className={`flex uppercase mt-2  text-sm ${jetBrainsMono.className} items-center text-center ${choice.effects.morale > 0  ? "text-[#00E639]" : "text-[#FFB4AB]"} `}>
+                <p className="w-28 text-left">Morale</p>
+                <p className="w-28 text-left">
+                    {choice.effects.morale > 0 ? "+" : ""}
+                    {choice.effects.morale}
+                </p>
+            </div>
+            <div className={`flex uppercase mt-2 text-sm ${jetBrainsMono.className} items-center text-center ${choice.effects.runway > 0  ? "text-[#00E639]" : "text-[#FFB4AB]"} `}>
+                <p className="w-28 text-left">Runway</p>
+                <p className="w-28 text-left">
+                    {choice.effects.runway > 0 ? "+" : ""}
+                    {choice.effects.runway} months
+                </p>
+            </div>
+            <button className={` bottom-12 flex gap-1 text-center justify-center cursor-pointer hover:scale-[90%] transition-all duration-300 items-center text-xl absolute rounded-lg bg-[#B3C5FF] px-5 py-3  font-medium text-[#0a0a0a] ${jetBrainsMono.className}`} onClick={onNext}>Next
+                <span>
+                    <ChevronRight />
+                </span>
+            </button>
+        </div>
+    )
+}
